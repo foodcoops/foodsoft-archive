cab80415010b2e24ad45c9c4c3e729512db19fed
sandoz <sandoz@jpberlin.de>
2009-03-24 22:39:24 +0100

    Revert "Second (last) part of multiple foodcoop installation."
    This reverts commit ffc3adf9072d542c34eb6fe45b6e20a80c16b4dc.
    
    Conflicts:
    
    	app/controllers/application.rb
    
    Tried to solve conflict.
    That commit breaks the whole application for me.

--- config/initializers/load_app_config.rb
+++ config/initializers/load_app_config.rb
@@ -1,15 +1,13 @@
-# Loads and returns config and databases for selected foodcoop.
+raw_config = File.read(RAILS_ROOT + "/config/app_config.yml")
+APP_CONFIG = YAML.load(raw_config)[RAILS_ENV].symbolize_keys
+
 module Foodsoft
   @@configs = YAML.load(File.read(RAILS_ROOT + "/config/app_config.yml"))
   @@databases = YAML.load(File.read(RAILS_ROOT + "/config/database.yml"))
   @@env = RAILS_ENV
 
-  def env=(env)
+  def set_env(env)
     @@env = env
-  end
-
-  def env
-    @@env
   end
 
   def config(rails_env = @@env)
@@ -26,6 +24,6 @@ end
 
 # Configuration of the exception_notification plugin
 # Mailadresses are set in config/foodsoft.yaml
-ExceptionNotifier.exception_recipients = Foodsoft.config[:notification]['error_recipients']
-ExceptionNotifier.sender_address = Foodsoft.config[:notification]['sender_address']
-ExceptionNotifier.email_prefix = Foodsoft.config[:notification]['email_prefix']
+ExceptionNotifier.exception_recipients = APP_CONFIG[:notification]['error_recipients']
+ExceptionNotifier.sender_address = APP_CONFIG[:notification]['sender_address']
+ExceptionNotifier.email_prefix = APP_CONFIG[:notification]['email_prefix']
