e3a7c4b9227de8cc50737c4183f937d9d354e6b0
wvengen <dev-generic@willem.engen.nl>
2014-09-24 18:13:35 +0200

    slightly larger database seed

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ Foodsoft/Sharedlists rebuild tryout 02
 This is a tryout of rebuilding Foodsoft/Sharedlists from scratch using [active_admin](activeadmin.info). It's not finished.
 
 Initially generated with [rails_apps_composer](https://github.com/RailsApps/rails_apps_composer).
+
+
+The seed contains some basic sample data; untar the file `seed_uploads.tar.gz` to unpack the associated images (you may need to fix permissions afterwards).
--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,23 @@ user = User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => EN
 puts 'user: ' << user.name
 user.confirm!
 user.add_role :admin
+
+# very basic sample data
+Article.create!([
+  {supplier_id: 1, name: "Aardbanaan", order_number: "", description: "", manufacturer: "", origin: "", url: "", image: nil, article_category_id: 2, price_id: nil}
+])
+ArticleCategory.create!([
+  {name: "Ingrediënten", icon: nil, parent_id: nil, lft: 1, rgt: 8, depth: 0},
+  {name: "Melk", icon: nil, parent_id: 1, lft: 2, rgt: 3, depth: 1},
+  {name: "Gluten", icon: nil, parent_id: 1, lft: 4, rgt: 5, depth: 1},
+  {name: "Soja", icon: nil, parent_id: 1, lft: 6, rgt: 7, depth: 1}
+])
+Supplier.create!([
+  {name: "CoolBoer", stype: "farm", email: "cool@boer.test", address: "Damrak, Amsterdam, Nederland", latitude: "52.3752124", longitude: "4.8958764", phone: "1234567890", phone2: "", fax: "", url: "", vat_number: "", chamber_number: "", logo: "farmer.jpeg"},
+  {name: "Willem & Drees", stype: "farm", email: "willemdrees@example.com", address: "Graaf van Lynden van Sandenburgweg 6, Cothen", latitude: "52.0006651", longitude: "5.3034849", phone: "0343 767001", phone2: "", fax: "", url: "http://www.willemendrees.nl/", vat_number: "", chamber_number: "", logo: "willem_en_drees.png"},
+  {name: "Brouwerij de Prael", stype: "brewery", email: "deprael@example.com", address: "Oudezijds Voorburgwal 30, 1012 GD, Amsterdam", latitude: "52.3752963", longitude: "4.8996704", phone: "020 4084470", phone2: "", fax: "", url: "http://www.deprael.nl/", vat_number: "", chamber_number: "", logo: "de_prael.png"},
+  {name: "BioRomeo", stype: "coop", email: "bioromeo@example.com", address: "Zwijnsweg 5, 8307 PP, Ens", latitude: "52.6630527", longitude: "5.838223", phone: "06 27650002", phone2: "", fax: "", url: "http://www.bioromeo.nl/", vat_number: "", chamber_number: "", logo: "BioRomeo.jpg"}
+])
+ArticlePrice.create!([
+  {article_id: 1, unit: "500g", unit_quantity: 1, price: "2.5", deposit: "0.0", tax: "6.0"}
+])
